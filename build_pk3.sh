a35a3f94188fe73ab29c00727f97a816e9aefb9e
sidav <sidav94@gmail.com>
2024-10-14 00:56:28 +0200

    Update the build script.

--- build_pk3.sh
+++ build_pk3.sh
@@ -1,1 +1,6 @@
-zip -r doomablo_$(date '+%d-%m-%Y_%H').pk3 * -x .git/**\*
+if [ $# -eq 0 ]
+    then
+        zip -r doomablo_$(git describe --exact-match --tags 2> /dev/null || git rev-parse --short HEAD)_$(date '+%d-%m-%Y_%H').pk3 * -x *.sh *.pk3 .git/**\*
+    else
+        zip -r doomablo_$1.pk3 * -x *.sh *.pk3 .git/**\*
+fi
